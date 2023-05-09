c6f23c89f543dc76ff282383827106b8b10740a6
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-09 16:07:43 +0000

    fixes

--- Theme/Backend/checkout-stripe.tpl.php
+++ Theme/Backend/checkout-stripe.tpl.php
@@ -5,7 +5,7 @@ $item = $request->getData('item');
 <div class="container">
     <h1>Checkout</h1>
     <div class="item">
-        <h2><?= $item->getL11n('name1')->description; ?></h2>
+        <h2><?= $item->getL11n('name1')->content; ?></h2>
         <p>Price: <strong><?= $item->salesPrice->getCurrency(); ?></strong></p>
 
     </div>
