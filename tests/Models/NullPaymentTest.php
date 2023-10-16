286fa9816f59c40cfae5ef2793582595be09854b
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:25:58 +0000

    auto generate NullTests

---
+++ tests/Models/NullPaymentTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Payment\tests\Models;
+
+use Modules\Payment\Models\NullPayment;
+
+/**
+ * @internal
+ */
+final class NullPaymentTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Payment\Models\NullPayment
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Payment\Models\Payment', new NullPayment());
+    }
+
+    /**
+     * @covers Modules\Payment\Models\NullPayment
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullPayment(2);
+        self::assertEquals(2, $null->getId());
+    }
+
+    /**
+     * @covers Modules\Payment\Models\NullPayment
+     * @group framework
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullPayment(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
