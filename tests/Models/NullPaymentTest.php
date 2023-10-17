ec02db5e91e2be496998434b174edfb2fc09d594
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullPaymentTest.php
+++ tests/Models/NullPaymentTest.php
@@ -47,6 +47,6 @@ final class NullPaymentTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullPayment(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
