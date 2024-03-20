b4aec23ec88349275623ee2165f47b7c585f07d4
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-20 03:00:24 +0000

    fix tests

--- tests/Models/NullPaymentTest.php
+++ tests/Models/NullPaymentTest.php
@@ -22,7 +22,7 @@ use Modules\Payment\Models\NullPayment;
 final class NullPaymentTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @covers Modules\Payment\Models\NullPayment
+     * @covers \Modules\Payment\Models\NullPayment
      * @group module
      */
     public function testNull() : void
@@ -31,7 +31,7 @@ final class NullPaymentTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\Payment\Models\NullPayment
+     * @covers \Modules\Payment\Models\NullPayment
      * @group module
      */
     public function testId() : void
@@ -41,7 +41,7 @@ final class NullPaymentTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @covers Modules\Payment\Models\NullPayment
+     * @covers \Modules\Payment\Models\NullPayment
      * @group module
      */
     public function testJsonSerialize() : void
