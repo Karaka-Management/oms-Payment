205c1bd3a6702454c6a9e5a45e82d502dad577e6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:15 +0200

    fix version and bugs

--- Models/NullPayment.php
+++ Models/NullPayment.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Payment\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Payment\Models;
  * Null model
  *
  * @package Modules\Payment\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Models/PaymentMapper.php
+++ Models/PaymentMapper.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Payment\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -21,7 +21,7 @@ use phpOMS\DataStorage\Database\Mapper\DataMapperFactory;
  * Account mapper class.
  *
  * @package Modules\Payment\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  *
